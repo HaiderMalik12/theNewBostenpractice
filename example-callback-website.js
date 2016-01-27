5bbe01f491d8375c9b11eca9304449b9289fdf69
=Haider Malik <haidermalik5044@gmail.com>
2016-01-27 17:24:27 +0500

    Add comsats portal callback example with node js

---
+++ example-callback-website.js
@@ -0,0 +1,31 @@
+/**
+ * Created by HaiderNazir on 1/27/2016.
+ */
+
+//example
+//lets suppose 100 users will want to check the marks from the portal
+
+//Server will get the request from user
+function getRequestFromUsers(clientName){
+    console.log('Request come from : ' +clientName );
+
+    checkMarksThroughPortalDb(function(){
+        console.log(clientName+' checked marks in '+'5 seconds');
+    });
+
+}
+
+//It will take 5 seconds to load the record and check marks for each user
+function checkMarksThroughPortalDb(callback){
+
+    setTimeout(callback,5000);
+}
+
+getRequestFromUsers('Haider');
+getRequestFromUsers('Mahnoor');
+getRequestFromUsers('Alishba');
+getRequestFromUsers('Faham');
+getRequestFromUsers('Tahir');
+getRequestFromUsers('Danial');
+getRequestFromUsers('Ghayuur');
+getRequestFromUsers('Moiz');
